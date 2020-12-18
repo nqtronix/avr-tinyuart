e59fd7446e0973d1e3fb4cf2d8aaf1d0a02f20f7
nqtronix <nqtronix@gmx.net>
2020-12-18 09:38:49 +0100

    Prepare for release

--- main.c
+++ main.c
@@ -19,25 +19,37 @@
 // Demo code to check if selected baud rate works on device
 int main(void)
 {
+	// disable the default div8 prescaler
+	CLKPR = 1<<CLKPCE;
+	CLKPR = 0b0000<<CLKPS0;
+	
 	// initializes IO
 	tinyuart_init();
 	_delay_us(10);
+	// interrupts must be disabled while using the send function
+	// cli();
+
 	// toggle pattern test: after every bit delay the TX pin is toggled
-	tinyuart_send_uint8(0b01010101);
+	tinyuart_send_uint8(0b01010101);	// = 0x55
  	_delay_us(10);
 
 	// every character test: verify that your receiver can read all chars at the chosen baud rate
 	// cheap UART-USB converts often have an issue with 0x00
 	for (uint16_t n = 0; n<256; n++)
 	{
-		tinyuart_send_uint8((uint8_t)(255-n));
+		tinyuart_send_uint8((uint8_t)(n));
 		_delay_us(10);
 	}
 	
 	// Test 0x00 to catch any glitches
-	while(1)
+	for (uint8_t n = 0; n<64; n++)
 	{
 		tinyuart_send_uint8(0);
 		_delay_us(1000);
 	}
+	
+	// re-enable interrupts
+	// sei();
+	
+	while(1);
 }
